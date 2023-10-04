5454dc390e56b46327071803d6b26a0d5e7d0de6
Peter Quach <pquach1@icloud.com>
2023-10-04 09:37:10 -0400

    Add Shopping List

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("This is my shopping list:\n" +
+                "1. Chill bros ice cream\n" +
+                "2. Protein powder\n" +
+                "3. Brussel Sprouts\n" +
+                "4. Broccoli\n" +
+                "5. Brown Rice\n" +
+                "6. Fresca Soda\n" +
+                "7. Lean Turkey\n" +
+                "8. Pork Chop\n" +
+                "9. Rotiserrie Chicken\n" +
+                "10. Sweet potatos\n");
+    }
+}
