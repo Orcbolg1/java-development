1b4780d29b81e6d13c899634f104e20ad43e5c84
Peter Quach <pquach1@icloud.com>
2023-10-10 11:54:24 -0400

    updated

--- workbook-2/FullNameGenerator/src/main/java/com/pluralight/FullNameAplication.java
+++ workbook-2/FullNameGenerator/src/main/java/com/pluralight/FullNameAplication.java
@@ -14,23 +14,36 @@ public class FullNameAplication {
         String last = lName(scanner);
         String suffix = sName(scanner);
 
-        System.out.println("Your Full Name is " + first + " " + middle + " " + last + " " + " " + suffix);
+
+        String fullName = first;
+        if(!middle.isEmpty()){
+            fullName += " " + middle.charAt(0) + ".";
+        }
+
+        fullName += " " + last;
+
+        if(!suffix.isEmpty()){
+            fullName += ", P" + suffix;
+        }
+
+        System.out.println("\nFullname is: " + fullName);
+
     }
 
     public static String fName(Scanner scanner){
         System.out.println("What is your First Name?: ");
-        return scanner.nextLine();
+        return scanner.nextLine().trim();
     }
     public static String mName(Scanner scanner) {
         System.out.println("What is your Middle Name?: ");
-        return scanner.nextLine();
+        return scanner.nextLine().trim();
     }
     public static String lName(Scanner scanner) {
         System.out.println("What is your Last Name?: ");
-        return scanner.nextLine();
+        return scanner.nextLine().trim();
     }
     public static String sName(Scanner scanner) {
         System.out.println("What is your Last Name?: ");
-        return scanner.nextLine();
+        return scanner.nextLine().trim();
     }
 }
