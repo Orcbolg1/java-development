c99a862c551091a4cc0b1d0ceecea3b83cb0583d
Peter Quach <pquach1@icloud.com>
2023-10-16 14:47:53 -0400

    Initial Commit

---
+++ workbook-3/PayrollCalcullator/src/main/java/com/pluralight/Employees.java
@@ -0,0 +1,53 @@
+package com.pluralight;
+
+public class Employees {
+
+    private int employeeID;
+    private String name;
+    private double hoursWorked;
+
+    private double payRate;
+
+    public Employees(int employeeID, String name, double hoursWorked, double payRate) {
+        this.employeeID = employeeID;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public double getGrossPay(){
+        return hoursWorked * payRate;
+    }
+}
--- workbook-3/PayrollCalcullator/src/main/java/com/pluralight/Main.java
+++ workbook-3/PayrollCalcullator/src/main/java/com/pluralight/Main.java
@@ -1,8 +1,34 @@
 package com.pluralight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FilterReader;
+
 public class Main {
 
     public static void main(String[] args) {
+        String fileName = "employees.csv";
 
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            String line;
+
+            while((line = bufferedReader.readLine()) != null){
+                String[] tokens = line.split("\\|");
+                int id = Integer.parseInt(tokens[0]);
+                String name = tokens[1];
+                double hoursWorked = Double.parseDouble(tokens[2]);
+                double payRate = Double.parseDouble(tokens[3]);
+
+                Employees employee = new Employees(id, name, hoursWorked, payRate);
+
+                System.out.printf("Employee ID: %d, Name: %s, Gross Pay: $%.2f%n", employee.getEmployeeID()
+                ,employee.getName(), employee.getGrossPay());
+            }
+
+
+        }catch(Exception ex){
+            
+        }
     }
 }
